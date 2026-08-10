e58f44d9670e078a9f0b0ba2633cd45eaf6671ba
Alexandre Agustini <alexandre.agustini@pucrs.br>
2026-08-10 13:10:57 +0000

    Refatorado para usar classe Ponto

--- 01-CirculosPrj/src/main/java/br/pucrs/poo/Circulo.java
+++ 01-CirculosPrj/src/main/java/br/pucrs/poo/Circulo.java
@@ -1,28 +1,37 @@
 package br.pucrs.poo;
 
 public class Circulo {
-    private double coordX;
-    private double coordY;
+    // private double coordX;
+    // private double coordY;
+    private Ponto centro;
     private double raio;
 
     public Circulo() {
-        this.coordX = 0;
-        this.coordY = 0;
-        this.raio = 1;
+        // this.coordX = 0;
+        // this.coordY = 0;
+        // this.centro = new Ponto(0,0);
+        // this.raio = 1;
+        this(0, 0, 1);
     }
 
-        public Circulo(double umX, double umY, double umRaio) {
-        this.coordX = umX;
-        this.coordY = umY;
+    public Circulo(double umX, double umY, double umRaio) {
+        // this.coordX = umX;
+        // this.coordY = umY;
+        this.centro = new Ponto(umX, umY);
         this.raio = umRaio;
     }
 
-    public void mover(double novoX, double novoY ) {
-        this.coordX = novoX;
-        this.coordY = novoY;
+    public void mover(double novoX, double novoY) {
+        // this.coordX = novoX;
+        // this.coordY = novoY;
+        // 1. criar um novo ponto
+        // this.centro = new Ponto(novoX, novoY);
+        // 2. usar o método mover do ponto
+        centro.mover(novoX, novoY);
+
     }
 
-    public void zoom(double fator){
+    public void zoom(double fator) {
         this.raio = this.raio * fator;
     }
 
@@ -33,11 +42,10 @@ public class Circulo {
     }
 
     @Override
-    public String toString(){
-        return "centro: " + this.coordX +
-               ", "       + this.coordY +
-               " raio: "  + this.raio   +
-               " area: "  + this.area();
+    public String toString() {
+        return "centro: " + centro.toString() +
+                " raio: " + this.raio +
+                " area: " + this.area();
 
     }
 
