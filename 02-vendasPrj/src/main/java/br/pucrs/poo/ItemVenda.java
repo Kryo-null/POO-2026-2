99830028f8bb517817d2172a5200f9048c856f79
Alexandre Agustini <alexandre.agustini@pucrs.br>
2026-08-12 13:31:26 +0000

    Estrutura básica do projeto Vendas

---
+++ 02-vendasPrj/src/main/java/br/pucrs/poo/ItemVenda.java
@@ -0,0 +1,7 @@
+package br.pucrs.poo;
+
+public class ItemVenda {
+    private int quantidade;
+    private Produto prod;
+
+}
---
+++ 02-vendasPrj/src/main/java/br/pucrs/poo/Venda.java
@@ -0,0 +1,9 @@
+package br.pucrs.poo;
+
+import java.util.ArrayList;
+
+public class Venda {
+    private Cliente cliente;
+    private ArrayList<ItemVenda> itens;
+
+}
